f7d34880372fe95659f7219771860227f2d7875e
Marcelo Eliseu <maeliseu@gmail.com>
2020-06-22 14:18:24 -0300

    style map

--- resources/views/maps.blade.php
+++ resources/views/maps.blade.php
@@ -2,53 +2,70 @@
 
 @section('main')
 
-    <div class="row">
-        <div class="col-12 mt-3">
-            <div class="card">
-                <div class="card-horizontal">
-                    <div class="img-square-wrapper">
-                        <img class="" src="{{$new_image}}" alt="Card image cap">
-                    </div>
-                    <div class="card-body">
-                        <h4 class="card-title">Mostragem de Casos por Região</h4>
-                        <div class="row" style="background: #E3E76C;">
-                            <div class="col-sm-9">CENTRAL - Região 1</div> <div class="col-sm-2">{{$casos->where('regiao','=','1')->count()}}</div>
-                        </div>
-                        <div class="row" style="background: #FCBD53;">
-                            <div class="col-sm-9">NORTE - Região 2</div> <div class="col-sm-2">{{$casos->where('regiao','=','2')->count()}}</div>
-                        </div>
-                        <div class="row" style="background: #41C6EF;">
-                            <div class="col-sm-9">OESTE - Região 3</div> <div class="col-sm-2">{{$casos->where('regiao','=','3')->count()}}</div>
-                        </div>
-                        <div class="row" style="background: #647BBD;">
-                            <div class="col-sm-9">SUL - Região 4</div> <div class="col-sm-2">{{$casos->where('regiao','=','4')->count()}}</div>
-                        </div>
-                        <div class="row" style="background: #E189B9;">
-                            <div class="col-sm-9">NORDESTE - Região 5</div> <div class="col-sm-2">{{$casos->where('regiao','=','5')->count()}}</div>
-                        </div>
-                        <div class="row" style="background: #888DC5;">
-                            <div class="col-sm-9">SUDESTE - Região 6</div> <div class="col-sm-2">{{$casos->where('regiao','=','6')->count()}}</div>
-                        </div>
-                        <div class="row" style="background: #FFF04E;">
-                            <div class="col-sm-9">SUDOESTE - Região 7</div> <div class="col-sm-2">{{$casos->where('regiao','=','7')->count()}}</div>
-                        </div>
-                        <div class="row" style="background: #93D0A7;">
-                            <div class="col-sm-9">LESTE I - Região 8</div> <div class="col-sm-2">{{$casos->where('regiao','=','8')->count()}}</div>
-                        </div>
-                        <div class="row" style="background: #EE5159;">
-                            <div class="col-sm-9">LESTE II - Região 9</div> <div class="col-sm-2">{{$casos->where('regiao','=','9')->count()}}</div>
-                        </div>
-                        <div class="row" style="">
-                            <div class="col-sm-9"><b>Total</b></div> <div class="col-sm-2"><b>{{$casos->count()}}</b></div>
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
+<body>
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 pt-4 pt-lg-0 order-2 order-lg-2 d-flex flex-column justify-content-center">
+          <h4 class="card-title">Mostragem de Casos por Região</h4>
+          <!-- -->
+          <div class="row p-1 pt-2 upsidelist" style="background: #E3E76C;">
+              <div class="col">Região 1 - CENTRAL</div>
+              <div class="col-1">{{$casos->where('regiao','=','1')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row p-1" style="background: #FCBD53;">
+              <div class="col">Região 2 - NORTE</div>
+              <div class="col-1">{{$casos->where('regiao','=','2')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row p-1" style="background: #41C6EF;">
+              <div class="col">Região 3 - OESTE</div>
+              <div class="col-1">{{$casos->where('regiao','=','3')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row p-1" style="background: #647BBD;">
+              <div class="col">Região 4 - SUL</div>
+              <div class="col-1">{{$casos->where('regiao','=','4')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row p-1" style="background: #E189B9;">
+              <div class="col">Região 5 - NORDESTE</div>
+              <div class="col-1">{{$casos->where('regiao','=','5')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row p-1" style="background: #888DC5;">
+              <div class="col">Região 6 - SUDESTE</div>
+              <div class="col-1">{{$casos->where('regiao','=','6')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row p-1" style="background: #FFF04E;">
+              <div class="col">Região 7 - SUDOESTE</div>
+              <div class="col-1">{{$casos->where('regiao','=','7')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row p-1" style="background: #93D0A7;">
+              <div class="col">Região 8 - LESTE I</div>
+              <div class="col-1">{{$casos->where('regiao','=','8')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row botsidelist p-1 pb-2" style="background: #EE5159;">
+              <div class="col">Região 9 - LESTE II</div>
+              <div class="col-1">{{$casos->where('regiao','=','9')->count()}}</div>
+          </div>
+          <!-- -->
+          <div class="row p-1" style="">
+              <div class="col"><b>Total</b></div>
+              <div class="col-1"><b>{{$casos->count()}}</b></div>
+          </div>
         </div>
+        <div class="col-lg-7 order-1 order-lg-1 hero-img">
+          <img src="{{$new_image}}" class="img-fluid" alt="">
+        </div>
+      </div>
     </div>
 
-@endsection
+  </section>
 
-
-
+  @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::Resource('casos', 'CasosController');
 Route::Resource('bairros', 'BairrosController');
